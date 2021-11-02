c2c62ef91c2c6d129e00b9b309a1bf682520cc83
Dorothy2612 <darriolaflores635@g.rwu.edu>
2021-11-02 17:53:43 -0400

    Programming assignment 6

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import sys
+def bigletter():
+    for item in (sys.argv):
+        print(item.capitalize())
+def n():
+    for item in (sys.argv):
+        print(item.count("is"))
 
- if __name__ == '__main__':
-   
+
+
+if __name__ == '__main__':
+    print(f"Arguments count: {len(sys.argv)}")
+    for item in (sys.argv):
+        print(item)
+    bigletter()
+    n()
+
+
 
